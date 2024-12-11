98f5a7bc5e0bfe96158ac503363b0649e249b227
graylwi2593 <graylwi2593@gmail.com>
2024-12-11 13:11:36 +0700

    Initial Commit

---
+++ Werewolf/WerewolfApp.swift
@@ -0,0 +1,17 @@
+//
+//  WerewolfApp.swift
+//  Werewolf
+//
+//  Created by Trịnh Kiết Tường on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WerewolfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
